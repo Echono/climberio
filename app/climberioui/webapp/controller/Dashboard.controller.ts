f2358fa478227f27459b325b131c0144fee73f13
Echono <wonger1324@gmail.com>
2026-01-31 13:58:19 +0100

    Changed main screen and added auth logic

--- app/climberioui/webapp/controller/Dashboard.controller.ts
+++ app/climberioui/webapp/controller/Dashboard.controller.ts
@@ -1,7 +1,6 @@
 import MessageBox from "sap/m/MessageBox";
 import BaseController from "./BaseController";
 import { Route$PatternMatchedEvent } from "sap/ui/core/routing/Route";
-import ODataModel from "sap/ui/model/odata/v4/ODataModel";
 
 /**
  * @namespace climberioui.controller
@@ -13,8 +12,21 @@ export default class Dashboard extends BaseController {
     }
 
     private async onPatternMatched(event: Route$PatternMatchedEvent) {
-        if(!await this.isAuthenticated()) {
-            this.navTo("main");
+        try {
+            this.getView().setBusy(true);
+            if(!await this.isAuthenticated()) {
+                const resourceBundle = await this.getResourceBundle();
+                throw new Error(resourceBundle.getText("notAuthenticatedError"));
+            }
+        } catch(error) {
+            MessageBox.error((error as Error).message, {
+                onClose: () => {
+                    this.getRouter().navTo("main");
+                }
+            }
+        );
+        } finally {
+            this.getView().setBusy(false);
         }
     }
 
--- app/climberioui/webapp/controller/Main.controller.ts
+++ app/climberioui/webapp/controller/Main.controller.ts
@@ -5,7 +5,20 @@ import { URLHelper } from "sap/m/library";
  * @namespace climberioui.controller
  */
 export default class Main extends BaseController {
+	
+	public onInit() {
+	this.getRouter().getRoute("main").attachPatternMatched(this.onRouteMatched, this);
+	}
+	
 	public loginWithGoogle() {
 		URLHelper.redirect("http://localhost:4004/auth/google");
 	}
+
+	public async onRouteMatched() {
+		if(await this.isAuthenticated()) {
+			this.getRouter().navTo("dashboard");
+		}
+	}
+
+
 }
