d9aa4c3762c915553b5877a3c70fc3a567feb5f0
Echono <wonger1324@gmail.com>
2026-02-07 11:58:35 +0100

    chnaged dashboard authentication check

--- app/climberioui/webapp/controller/Dashboard.controller.ts
+++ app/climberioui/webapp/controller/Dashboard.controller.ts
@@ -1,6 +1,5 @@
 import MessageBox from "sap/m/MessageBox";
 import BaseController from "./BaseController";
-import { Route$PatternMatchedEvent } from "sap/ui/core/routing/Route";
 import { SideNavigation$ItemPressEvent } from "sap/tnt/SideNavigation";
 
 /**
@@ -8,18 +7,25 @@ import { SideNavigation$ItemPressEvent } from "sap/tnt/SideNavigation";
  */
 export default class Dashboard extends BaseController {
 
-    public onInit() {
+    private authenticationCheck = false;
+
+    public onBeforeRendering() {
+        this.checkAuthentication();
         this.getRouter().getRoute("dashboard").attachPatternMatched(this.onPatternMatched, this);
     }
 
-    private async onPatternMatched(event: Route$PatternMatchedEvent) {
+    private async checkAuthentication() {
         try {
             this.getView().setBusy(true);
             if(!await this.isAuthenticated()) {
                 const resourceBundle = await this.getResourceBundle();
                 throw new Error(resourceBundle.getText("notAuthenticatedError"));
             }
-            this.navTo("home");
+            const currentRoute = this.getRouter().getHashChanger().getHash();
+            if(currentRoute === "dashboard") {
+                this.navTo("home");
+                this.authenticationCheck = true;
+            }
         } catch(error) {
             MessageBox.error((error as Error).message, {
                 onClose: () => {
@@ -29,6 +35,12 @@ export default class Dashboard extends BaseController {
         );
         } finally {
             this.getView().setBusy(false);
+        }
+    }
+
+    private onPatternMatched() {
+        if(this.authenticationCheck) {
+            this.navTo("home");
         }
     }
 
