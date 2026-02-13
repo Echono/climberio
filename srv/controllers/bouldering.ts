bd114f2f33f32c9a168aecf221d0456f8d28d819
Echono <wonger1324@gmail.com>
2026-02-13 20:19:20 +0100

    Created update of avreage rating for routes

--- srv/controllers/bouldering.ts
+++ srv/controllers/bouldering.ts
@@ -1,5 +1,5 @@
-import { Registration, Registrations } from "#cds-models/BoulderingService";
-import { connect, Request } from "@sap/cds";
+import { Registration, Registrations, Routes } from "#cds-models/BoulderingService";
+import cds, { connect, Request } from "@sap/cds";
 import BoulderingService from "../bouldering-srv";
 
 export const calculateAverageRating = async (subjects: any[], req: Request) => {
@@ -14,12 +14,16 @@ export const calculateAverageRating = async (subjects: any[], req: Request) => {
     // Connect to service
     const service = await connect.to(BoulderingService);
 
-    // loop through all registrations
-    for(const registraion of registrations) {
-        // get all ratings for the route
-        const ratings = await SELECT.from(Registrations).columns("runnerGradeDifficulty").where({ route_ID: registraion.route_ID }).bind(service);
-        
+    // Loop through all registrations
+    for(const registration of registrations) {
+        // Use cds.run to bypass authrization restrictions to get all ratings for current route
+        const ratings: Registrations = await cds.run(SELECT.from(Registrations).columns("runnerGradeDifficulty").where({ route_ID: registration.route_ID }));
+    
+        // Calc average rating
+        const averageRating = ratings.reduce((acc, curr) => acc + curr.runnerGradeDifficulty!, 0) / ratings.length;
+
+        // Update route with new average rating
+        await UPDATE(Routes).set({ averageRating: averageRating }).where({ ID: registration.route_ID }).bind(service);
     }
     
-
 }
