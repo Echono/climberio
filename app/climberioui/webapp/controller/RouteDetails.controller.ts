51d124328de92a1e1568b7cf8b9cd62fc01914e7
Echono <wonger1324@gmail.com>
2026-02-13 01:33:12 +0100

    Fixed import issues

--- app/climberioui/webapp/controller/RouteDetails.controller.ts
+++ app/climberioui/webapp/controller/RouteDetails.controller.ts
@@ -1,6 +1,9 @@
 import BaseController from "./BaseController";
 import { Route$PatternMatchedEvent } from "sap/ui/core/routing/Route";
 import List from "sap/m/List";
+import ODataListBinding from "sap/ui/model/odata/v4/ODataListBinding";
+import Filter from "sap/ui/model/Filter";
+import FilterOperator from "sap/ui/model/FilterOperator";
 
 /**
  * @namespace climberioui.controller
@@ -28,13 +31,17 @@ export default class RouteDetails extends BaseController {
         const view = this.getView();
         view.bindElement({
             path: `/RouteSet('${routeID}')`,
+            model: "bouldering",
             parameters: {
                 $expand: `tags($expand=tag)`
             }
         })
         const list = view.byId("routeDetailsMyRegistration") as List;
         const binding = list.getBinding("items") as ODataListBinding;
-        binding.filter(new Filter("route_ID", "EQ", routeID));
-        binding.resume();  
+        binding.filter(new Filter("route_ID", FilterOperator.EQ, routeID));
+        if(binding.isSuspended()) {
+            binding.resume();  
+        }
+
     }
 }
