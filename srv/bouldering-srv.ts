5ef39c969e8a6c305c2890b21ba8fbe5e7cf7d2f
Echono <wonger1324@gmail.com>
2026-02-13 03:14:14 +0100

    Started a avereage check

--- srv/bouldering-srv.ts
+++ srv/bouldering-srv.ts
@@ -1,11 +1,11 @@
-import { Routes } from "#cds-models/BoulderingService";
+import { Registrations } from "#cds-models/BoulderingService";
 import { ApplicationService } from "@sap/cds";
 import { calculateAverageRating } from "./controllers/bouldering";
 
 export default class BoulderingService extends ApplicationService {
     async init() {
 
-        this.after('READ', Routes, calculateAverageRating);
+        this.after(['POST', 'UPDATE', 'DELETE'], Registrations, calculateAverageRating);
 
         return super.init();
     }
--- srv/controllers/bouldering.ts
+++ srv/controllers/bouldering.ts
@@ -1,5 +1,25 @@
-import { OnEventHandler, Request } from "@sap/cds";
+import { Registration, Registrations } from "#cds-models/BoulderingService";
+import { connect, Request } from "@sap/cds";
+import BoulderingService from "../bouldering-srv";
 
-export const calculateAverageRating = async (results: any[], req: Request) => {
+export const calculateAverageRating = async (subjects: any[], req: Request) => {
+    // Turn subjects into a workable array
+    let registrations: Registrations = [];
+    if(Array.isArray(subjects)) {
+        registrations = subjects as Registrations;
+    } else {
+        registrations = [subjects as Registration];
+    }
+
+    // Connect to service
+    const service = await connect.to(BoulderingService);
+
+    // loop through all registrations
+    for(const registraion of registrations) {
+        // get all ratings for the route
+        const ratings = await SELECT.from(Registrations).columns("runnerGradeDifficulty").where({ route_ID: registraion.route_ID }).bind(service);
+        
+    }
+    
 
 }
